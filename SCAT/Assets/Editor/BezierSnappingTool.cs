f99c4f895a7de391d2b77ca3d6489ac745ffd951
Co6op13 <Co6op13@yandex.ru>
2021-10-15 16:49:38 +0500

    test

---
+++ SCAT/Assets/Editor/BezierSnappingTool.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.EditorTools;
+
+[EditorTool("Snap Bezier ", typeof (BezierSnapPoint))]
+public class BezierSnappingTool : MonoBehaviour
+{
+   
+    //public override void OnToolGUI(EditorWindow window)
+    //{
+    //    //Transform targetTransform = ((BezierSnapPoint)targetTransform).transform;
+    //}
+}
+
