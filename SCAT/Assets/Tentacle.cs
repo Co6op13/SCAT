66a603ca4814be4a8f891b7f269b43b9c1adcdc5
Co6op13 <Co6op13@yandex.ru>
2021-10-18 17:48:57 +0500

    добавляем тентакли

---
+++ SCAT/Assets/Tentacle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacle : MonoBehaviour
+{
+    [SerializeField] private int length;
+    [SerializeField] private float lengthSegment;
+    [SerializeField] private float smoothSpeed;
+    [SerializeField] private float trailSpeed;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Vector3[] segmentPosition;
+    [SerializeField] private Transform targetDirection;
+    
+
+    private Vector3[] segmentVelocity;
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = length;
+        segmentPosition = new Vector3[length];
+        segmentVelocity = new Vector3[length];
+    }
+
+    private void Update()
+    {
+        segmentPosition[0] = targetDirection.position;
+
+        for (int i = 1; i < segmentPosition.Length; i++)
+        {
+            segmentPosition[i] = Vector3.SmoothDamp(segmentPosition[i], segmentPosition[i - 1] + targetDirection.right * lengthSegment,ref segmentVelocity[i], smoothSpeed);
+        }
+
+        lineRenderer.SetPositions(segmentPosition);
+    }
+
+
+
+}
