a4ef4929b737e393b8fb51d757c7e756982de168
Co6op13 <Co6op13@yandex.ru>
2021-11-02 00:15:45 +0500

    верхняя турель отрабатывает правильно. остальыне нет

--- SCAT/Assets/TurretData.cs
+++ SCAT/Assets/TurretData.cs
@@ -11,29 +11,33 @@ public class TurretData : MonoBehaviour, iHP
     [SerializeField] internal Transform target;
     [SerializeField] private float timeBetweenSearch = 0.5f;
     [SerializeField] private float rangeMoveWeapon = 30;
+    [SerializeField] private Transform pivotWeapon;
+    [SerializeField] private float speedRotate;
     internal CompositeCollider2D collider2d;
     [SerializeField] private PlayersPool playersPool;
     private float time;
-    private float maxAnglePosiotonWeapon;
-    private float minAnglePosiotonWeapon;
-    [SerializeField] private Vector3 maxDirection;
-    [SerializeField] private Vector3 minDirection;
-    [SerializeField] private Vector3 targetDirection;
+    private float maxDirectionAngle;
+    private float minDirectionAngle;
+    private float targetDirectionAngle = 90;
+   
+   // [SerializeField] private Vector3 targetDirection = Vector3.up;
 
 
     private void Start()
     {
+        targetDirectionAngle = transform.eulerAngles.z + 90;
         playersPool = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PlayersPool>();
-        time = Time.time + timeBetweenSearch;
+        time = Time.time + 1f;
+        target = playersPool.GetNearestTarger(transform.position);
         GetRangePositionWeapon();
     }
 
     private void GetRangePositionWeapon()
     {
-        maxAnglePosiotonWeapon = transform.rotation.z + 90 + rangeMoveWeapon;
-        minAnglePosiotonWeapon = transform.rotation.z + 90 - rangeMoveWeapon;
-        maxDirection = GetVectorFromAngle(maxAnglePosiotonWeapon);
-        minDirection = GetVectorFromAngle(minAnglePosiotonWeapon);       
+       
+        maxDirectionAngle = transform.eulerAngles.z + 90 + rangeMoveWeapon;
+        minDirectionAngle = transform.eulerAngles.z + 90 - rangeMoveWeapon;
+      
     }
 
     private void FixedUpdate()
@@ -46,22 +50,34 @@ public class TurretData : MonoBehaviour, iHP
                 target = playersPool.GetNearestTarger(transform.position);
             }
             time = Time.time + timeBetweenSearch;
+            targetDirectionAngle = GetDirection(target);
         }
 
-        targetDirection = GetDirection(target);
-        MoveWeapon();
+        Debug.DrawRay(transform.position, GetVectorFromAngle(targetDirectionAngle) * 5, Color.red);    
+        MoveWeapon(targetDirectionAngle);
     }
 
-    private Vector3 GetDirection(Transform target)
+    private float GetDirection(Transform target)
     {
         Vector3 direction = (target.position - transform.position).normalized;
-        Debug.DrawLine(transform.position, direction + transform.position, Color.red);
-        return direction;
+        float directionAngle = GetAngleFromVectorFloat(direction);
+        if (directionAngle < minDirectionAngle)
+        {
+            return minDirectionAngle;
+        }
+        else if ( directionAngle > maxDirectionAngle)
+        {
+            return maxDirectionAngle;
+        }
+        else
+            return directionAngle;
     }
 
-    void MoveWeapon()
+    void MoveWeapon( float directionAngle)
     {
 
+        pivotWeapon.localRotation = Quaternion.Slerp(pivotWeapon.localRotation,
+            Quaternion.Euler(0f,0f , directionAngle + 90), speedRotate * Time.fixedDeltaTime);
     }
     public int GetMaxHP()
     {
@@ -70,8 +86,8 @@ public class TurretData : MonoBehaviour, iHP
 
     private void OnDrawGizmos()
     {
-        Debug.DrawLine(transform.position, maxDirection + transform.position);
-        Debug.DrawLine(transform.position, minDirection + transform.position);
+        Debug.DrawRay(transform.position, GetVectorFromAngle( maxDirectionAngle) * 3 , Color.gray);
+        Debug.DrawRay(transform.position, GetVectorFromAngle( minDirectionAngle) * 3 , Color.gray);
     }
 
     public static Vector3 GetVectorFromAngle(float angle)
@@ -79,5 +95,15 @@ public class TurretData : MonoBehaviour, iHP
         // angle = 0 -> 360
         float angleRad = angle * (Mathf.PI / 180f);
         return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+
+    public static float GetAngleFromVectorFloat(Vector3 direction)
+    {
+        direction = direction.normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (angle < 0) angle += 360;
+
+        return angle;
     }
 }
